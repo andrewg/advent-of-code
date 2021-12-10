f18806bc712cb3f1ed1075fb51082629de1c4869
Andrew Gniadek <andrew@gniadek.ca>
2021-12-10 02:35:46 -0800

    Fix null checks

--- util/src/main/kotlin/ca/gniadek/aoc/util/Resources.kt
+++ util/src/main/kotlin/ca/gniadek/aoc/util/Resources.kt
@@ -3,8 +3,8 @@ package ca.gniadek.aoc.util
 fun readInput(name: String = "input"): Sequence<String> {
     return sequence {
         this::class.java.classLoader.getResourceAsStream(name).use { input ->
-            input.bufferedReader().use { reader ->
-                reader.lineSequence().forEach {
+            input?.bufferedReader().use { reader ->
+                reader?.lineSequence()?.forEach {
                     yield(it)
                 }
             }
